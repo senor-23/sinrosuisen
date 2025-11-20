39c35997ef6c3cbca7790af020947c0304ed6fcb
senor-23 <nakasena.2312@gmail.com>
2025-11-20 16:28:52 +0900

    Update app1.py

--- app1.py
+++ app1.py
@@ -6,6 +6,6 @@ name = st.text_input("好きな言葉を入力してください")
 
 st.write(name)
 
-camera_photo = st.camera_input("写真を撮影します！")
+camera = st.camera_input("写真を撮影します！")
 if camera:
     st.image(camera, caption="写真", use_column_width=True)
